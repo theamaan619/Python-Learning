1aac53672fd29fb6c1000e4b8fa0e7f6481d71cc
theamaan619 <163080347+theamaan619@users.noreply.github.com>
2025-07-24 12:51:14 +0530

    Delete CorePython directory

--- CorePython/14_exceptions.py
+++
@@ -1,9 +0,0 @@
-# Exceptions are used to handle errors gracefully
-
-try:
-    x = int(input("Enter a number: "))
-    print("You entered:", x)
-except ValueError:
-    print("That's not a valid number!")
-finally:
-    print("End of program.")
